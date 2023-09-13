e07f23d7113906e381547641ab95b523057a7fcd
Vishwajith <vishwajith.ss@gmail.com>
2023-09-13 11:29:12 +0530

    updated bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'hex': "url('/hexbg.png')",
+        'hex': "url('/hexbgv2.png')",
       },
       colors:{
         'nav-color': "rgba(35, 33, 33, 0.72)",
