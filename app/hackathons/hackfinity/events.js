9672ff9025d3737c17c2e98f63834eb9ef39fa4c
Gokulram A <gokulram2010418@ssn.edu.in>
2023-09-27 00:57:51 +0530

    hackathons details update

--- app/hackathons/hackfinity/events.js
+++ app/hackathons/hackfinity/events.js
@@ -1,41 +1,66 @@
 export const HACKFINITEvent = {
   name: 'Hackinfinity',
-  description:
-    "The flagship event of the Invente, emerges as a celebration of ingenuity, collaboration, and relentless passion for problem-solving. Problem statements are chosen in the fields of Healthcare ,Renewable Energy, Fintech , Sustainability, Smart cities, supply chain and logistics, Agritech, Education and Tourism. It's not just another hackathon, it is a whirlwind of imagination and coding prowess that unfolds over the course of 24 hours where aspiring developers, designers, and creators converge to transform their ideas into reality. General instructions: 1.All the team members must have proper internet connection. 2.Participants are required to have their own components and software in order to implement their solution (including extension boxes). 3.Each team will be allocated separate mentors. 4.Mentorship for technical skills will be provided. 5.Participants are supposed to build their product/solution during the Hackathon. 6.Participants are allowed to use existing libraries or components, however only the work done during the 24 hrs will be considered for evaluation. 7.Solutions/products can be a mix of hardware and software technologies, but can be purely software or hardware based as well. 8.Participants will be informed of the order in which reviews will happen and must attend their meetings at the specified times. 9.Solutions/products will be judged based on Innovation, Impact, Feasibility and Marketability. 10.Organizers will be available for any further help/queries.",
+  description: `The flagship event of the Invente, emerges as a celebration of ingenuity, collaboration, and relentless passion for problem-solving. Problem statements are chosen in the fields of Healthcare ,Renewable Energy, Fintech , Sustainability, Smart cities, supply chain and logistics, Agritech, Education and Tourism. It's not just another hackathon, it is a whirlwind of imagination and coding prowess that unfolds over the course of 24 hours where aspiring developers, designers, and creators converge to transform their ideas into reality.   
+    **General instructions:**  
+    1.All the team members must have proper internet connection.  
+    2.Participants are required to have their own components and software in order to implement their solution (including extension boxes).  
+    3.Each team will be allocated separate mentors.  
+    4.Mentorship for technical skills will be provided.  
+    5.Participants are supposed to build their product/solution during the Hackathon.  
+    6.Participants are allowed to use existing libraries or components, however only the work done during the 24 hrs will be considered for evaluation.  
+    7.Solutions/products can be a mix of hardware and software technologies, but can be purely software or hardware based as well.  
+    8.Participants will be informed of the order in which reviews will happen and must attend their meetings at the specified times.  
+    9.Solutions/products will be judged based on Innovation, Impact, Feasibility and Marketability. 10.Organizers will be available for any further help/queries.`,
   type: 'team',
-  event_type: 'Semi-Technical',
+  event_type: 'hackathon',
   team_size: '2-3',
   department: 'ECE',
   rounds: [
     {
       name: ' Domains for Problem Statement',
-      description:
-        '1.Healthcare technologies. 2.Fintech. 3.Renewable energy and sustainability. 4.Smart cities and infrastructure. 5.Education technology. 6.Agritech. 7.Supply chain and logistics. 8.Student innovation (open idea)',
+      description: `1.Healthcare technologies.  
+        2.Fintech.  
+        3.Renewable energy and sustainability.  
+        4.Smart cities and infrastructure.  
+        5.Education technology  
+        6.Agritech.  
+        7.Supply chain and logistics.  
+        8.Student innovation (open idea)`,
     },
     {
       name: 'Schedule',
-      description:
-        'Day 1: ● 10.30 AM – Introduction. ● 11.00 AM – Hackathon begins. ● 03.00 PM – First Review. ● 9.00 PM – Second Review. Day 2: ● 08.00 AM – Third Review. ● 11.00 AM – Hackathon ends. ● 12.00 PM – Final Presentation. ● 03.30 PM – Valedictory.',
+      description: `
+        Day 1:  
+            ● 10.30 AM – Introduction.  
+            ● 11.00 AM – Hackathon begins.  
+            ● 03.00 PM – First Review.  
+            ● 9.00 PM – Second Review.  
+        Day 2:   
+            ● 08.00 AM – Third Review.  
+            ● 11.00 AM – Hackathon ends.  
+            ● 12.00 PM – Final Presentation.  
+            ● 03.30 PM – Valedictory.`,
     },
     {
       name: 'First review',
-      description:
-        '1.Teams are expected to explain their problem statement, background study done and the workflow of their solution. 2.Teams will be judged on the novelty of their problem statement and the clarity in their approach moving forward. 3.Advice on how to proceed further will be provided.',
+      description: `1.Teams are expected to explain their problem statement, background study done and the workflow of their solution.   
+        2.Teams will be judged on the novelty of their problem statement and the clarity in their approach moving forward.    
+        3.Advice on how to proceed further will be provided.`,
     },
     {
       name: 'Second review',
-      description:
-        '1. This review is a checkpoint to track the team’s progress. 2. The reviewers will give their input and their thoughts on the team’s solution and changes if there any are warranted.',
+      description: `1.This review is a checkpoint to track the team’s progress.  
+        2. The reviewers will give their input and their thoughts on the team’s solution and changes if there any are warranted.`,
     },
     {
       name: 'Third review',
-      description:
-        '1. Teams are expected to implement the changes given in the previous review by the reviewers. 2. In this round, the teams are reviewed based on their progress in accordance with their proposed workflow and closeness to completion of the project.',
+      description: `1.Teams are expected to implement the changes given in the previous review by the reviewers.  
+      2. In this round, the teams are reviewed based on their progress in accordance with their proposed workflow and closeness to completion of the project.`,
     },
     {
       name: 'Final Presentation:',
-      description:
-        '1. In this round, teams will be evaluated based on presentation skills, ability to explain their idea in a lucid manner and the degree of innovation in their solution. 2. Teams which address the marketability of their product will be awarded additional points. The hackathon will follow a leader board system. The final score will be the cumulative points gained by the team over the three reviews and final presentation. The team with the highest score will be announced the winner of the hackathon. Decisions taken by the review panel will be final in all regards.',
+      description: `1.In this round, teams will be evaluated based on presentation skills, ability to explain their idea in a lucid manner and the degree of innovation in their solution.  
+        2. Teams which address the marketability of their product will be awarded additional points. The hackathon will follow a leader board system. The final score will be the cumulative points gained by the team over the three reviews and final presentation. The team with the highest score will be announced the winner of the hackathon. Decisions taken by the review panel will be final in all regards.`,
     },
   ],
   venue: '2nd Floor, CDC Building',
