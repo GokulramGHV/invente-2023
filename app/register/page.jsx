d5e4f1172156ba43dec38872670ee4064631c80c
Gokulram A <gokulram2010418@ssn.edu.in>
2023-09-30 12:34:45 +0530

    register page nav fix

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -27,88 +27,85 @@ const rulesArray = [
 
 export default function Register() {
   return (
-    <>
-      <Navbar />
-      <div className="bg-hex min-h-screen grid xl:grid-cols-2 w-full pt-10 pb-24">
-        <div className="flex flex-col xl:flex-row h-full w-full">
-          <div className="xl:px-20 pt-10 xl:pt-0 px-8 h-full flex-1 flex flex-col justify-center items-center relative">
-            <Image
-              src="/invente-bulb.png"
-              alt="invente-bulb"
-              width={300}
-              height={300}
-              className="absolute top-[40%] translate-y-[-50%] left-[15%] opacity-30 -rotate-[16deg] z-[0]"
-            />
-            <h1
-              className={`${bayon.className} xl:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#BAF7FF]`}
-            >
-              REGISTER NOW!
-            </h1>
-            <p className="text-center mt-2">
-              Make sure you have read the rules and regulations before you pay
-              for the passes.
-            </p>
-            <div className="xl:mt-20 mt-10 flex gap-4 relative xl:right-6 right-3">
-              <div className="flex flex-col gap-2 justify-center items-center w-fit">
-                <h3 className="text-4xl font-semibold">1</h3>
-                <div className="h-8 w-1 rounded-full bg-white"></div>
-                <h3 className="text-4xl font-semibold">2</h3>
-                <div className="h-8 w-1 rounded-full bg-white"></div>
-                <h3 className="text-4xl font-semibold">3</h3>
+    <div className="bg-hex min-h-screen grid xl:grid-cols-2 w-full pt-10 pb-24">
+      <div className="flex flex-col xl:flex-row h-full w-full">
+        <div className="xl:px-20 pt-10 xl:pt-0 px-8 h-full flex-1 flex flex-col justify-center items-center relative">
+          <Image
+            src="/invente-bulb.png"
+            alt="invente-bulb"
+            width={300}
+            height={300}
+            className="absolute top-[40%] translate-y-[-50%] left-[15%] opacity-30 -rotate-[16deg] z-[0]"
+          />
+          <h1
+            className={`${bayon.className} xl:text-7xl text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#FFFFFF] to-[#BAF7FF]`}
+          >
+            REGISTER NOW!
+          </h1>
+          <p className="text-center mt-2">
+            Make sure you have read the rules and regulations before you pay for
+            the passes.
+          </p>
+          <div className="xl:mt-20 mt-10 flex gap-4 relative xl:right-6 right-3">
+            <div className="flex flex-col gap-2 justify-center items-center w-fit">
+              <h3 className="text-4xl font-semibold">1</h3>
+              <div className="h-8 w-1 rounded-full bg-white"></div>
+              <h3 className="text-4xl font-semibold">2</h3>
+              <div className="h-8 w-1 rounded-full bg-white"></div>
+              <h3 className="text-4xl font-semibold">3</h3>
+            </div>
+            <div className="flex flex-col gap-10">
+              <div className="w-full flex items-center gap-5">
+                <a
+                  target="_blank"
+                  href="https://rzp.io/l/Invente-23"
+                  className="get-passes-button text-white text-base font-bold whitespace-nowrap xl:px-20 px-10 py-3 transition-all duration-200 ease-in-out uppercase w-full cursor-pointer text-center"
+                >
+                  Pay Now
+                </a>
               </div>
-              <div className="flex flex-col gap-10">
-                <div className="w-full flex items-center gap-5">
-                  <a
-                    target="_blank"
-                    href="https://rzp.io/l/Invente-23"
-                    className="get-passes-button text-white text-base font-bold whitespace-nowrap xl:px-20 px-10 py-3 transition-all duration-200 ease-in-out uppercase w-full cursor-pointer text-center"
-                  >
-                    Pay Now
-                  </a>
-                </div>
-                <div className="w-full flex items-center gap-5">
-                  <a
-                    target="_blank"
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSc4mr5QFS-GT45HPNmx4ZkGw-XsWgISVWx4m0Crg1E4IpmiVw/viewform?vc=0&c=0&w=1&flr=0"
-                    className="get-passes-button text-white text-base font-bold whitespace-nowrap xl:px-20 px-10 py-3 transition-all duration-200 ease-in-out uppercase w-full cursor-pointer text-center"
-                  >
-                    Register Here
-                  </a>
-                </div>
-                <div className="w-full flex items-center gap-5">
-                  <a
-                    target="_blank"
-                    href="https://linktr.ee/ssnsnucinvente23"
-                    className="get-passes-button text-white text-base font-bold whitespace-nowrap xl:px-20 px-10 py-3 transition-all duration-200 ease-in-out uppercase w-full cursor-pointer text-center"
-                  >
-                    Event Forms
-                  </a>
-                </div>
+              <div className="w-full flex items-center gap-5">
+                <a
+                  target="_blank"
+                  href="https://docs.google.com/forms/d/e/1FAIpQLSc4mr5QFS-GT45HPNmx4ZkGw-XsWgISVWx4m0Crg1E4IpmiVw/viewform?vc=0&c=0&w=1&flr=0"
+                  className="get-passes-button text-white text-base font-bold whitespace-nowrap xl:px-20 px-10 py-3 transition-all duration-200 ease-in-out uppercase w-full cursor-pointer text-center"
+                >
+                  Register Here
+                </a>
+              </div>
+              <div className="w-full flex items-center gap-5">
+                <a
+                  target="_blank"
+                  href="https://linktr.ee/ssnsnucinvente23"
+                  className="get-passes-button text-white text-base font-bold whitespace-nowrap xl:px-20 px-10 py-3 transition-all duration-200 ease-in-out uppercase w-full cursor-pointer text-center"
+                >
+                  Event Forms
+                </a>
               </div>
             </div>
           </div>
-          <div
-            className="xl:h-full xl:w-[1px] w-full h-[1px] opacity-80 my-10 xl:my-0"
-            style={{
-              background:
-                'radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0) 0%, #FFFFFF 52.6%, rgba(255, 255, 255, 0) 100%',
-            }}
-          ></div>
         </div>
-        <div className="flex flex-col justify-center items-center xl:px-28 px-8">
-          <h2 className={`${bayon.className} xl:text-6xl text-5xl uppercase`}>
-            Rules
-          </h2>
-          <div className="text-lg space-y-3 mt-6">
-            {rulesArray.map((rule, i) => (
-              <div key={i} className="flex gap-3">
-                <h3 className="font-semibold">{i + 1}. </h3>
-                <p className="leading-6">{rule}</p>
-              </div>
-            ))}
-          </div>
+        <div
+          className="xl:h-full xl:w-[1px] w-full h-[1px] opacity-80 my-10 xl:my-0"
+          style={{
+            background:
+              'radial-gradient(50% 50% at 50% 50%, rgba(255, 255, 255, 0) 0%, #FFFFFF 52.6%, rgba(255, 255, 255, 0) 100%',
+          }}
+        ></div>
+      </div>
+      <div className="flex flex-col justify-center items-center xl:px-28 px-8">
+        <h2 className={`${bayon.className} xl:text-6xl text-5xl uppercase`}>
+          Rules
+        </h2>
+        <div className="text-lg space-y-3 mt-6">
+          {rulesArray.map((rule, i) => (
+            <div key={i} className="flex gap-3">
+              <h3 className="font-semibold">{i + 1}. </h3>
+              <p className="leading-6">{rule}</p>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 }
